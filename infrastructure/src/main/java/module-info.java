43d746ff538aa6f1c6fc59dc2aeff6f59dec0ee3
Pál Kecskeméti <kecskemetip@gmail.com>
2021-08-02 20:10:38 +0200

    Using org.javamodularity:moduleplugin

--- infrastructure/src/main/java/module-info.java
+++ infrastructure/src/main/java/module-info.java
@@ -1,11 +1,13 @@
 module com.baeldung.dddmodules.infrastructure {
-	requires transitive com.baeldung.dddmodules.sharedkernel;
-	requires transitive com.baeldung.dddmodules.ordercontext;
-	requires transitive com.baeldung.dddmodules.shippingcontext;
+	requires com.baeldung.dddmodules.sharedkernel;
+	requires com.baeldung.dddmodules.ordercontext;
+	requires com.baeldung.dddmodules.shippingcontext;
 	provides com.baeldung.dddmodules.sharedkernel.events.EventBus
 			with com.baeldung.dddmodules.infrastructure.events.SimpleEventBus;
 	provides com.baeldung.dddmodules.ordercontext.repository.CustomerOrderRepository
 			with com.baeldung.dddmodules.infrastructure.db.InMemoryOrderStore;
 	provides com.baeldung.dddmodules.shippingcontext.repository.ShippingOrderRepository
 			with com.baeldung.dddmodules.infrastructure.db.InMemoryOrderStore;
+	exports com.baeldung.dddmodules.infrastructure.db;
+	exports com.baeldung.dddmodules.infrastructure.events;
 }
--- shippingcontext/src/main/java/module-info.java
+++ shippingcontext/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.baeldung.dddmodules.shippingcontext {
+open module com.baeldung.dddmodules.shippingcontext {
 	requires com.baeldung.dddmodules.sharedkernel;
 	exports com.baeldung.dddmodules.shippingcontext.service;
 	exports com.baeldung.dddmodules.shippingcontext.model;
